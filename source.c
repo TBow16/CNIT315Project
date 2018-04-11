df229a487211b18e5580bbb97021f07636e3aad2
Kevin Chen <keverchen@gmail.com>
2018-04-11 19:47:29 -0400

    Update source.c

--- source.c
+++ source.c
@@ -1,1 +1,38 @@
 #include jsmn.h
+
+struct Node
+{
+  char username[100];
+  char message[255];
+  int count;
+  struct Node *next;
+};
+
+struct Node *CreateNode(char username[], char message[], int count)
+{
+    struct Node *newnode = (struct Node *)malloc(sizeof(struct Node));
+    if (NULL == newnode) 
+    { 
+        printf("Not enough memory left on the system. ;A;\n");
+    }
+    strcpy(newnode->username, username);
+    strcpy(newnode->message, message);
+    newnode->count = count;
+    newnode->next = NULL;
+};
+
+int main()
+{
+  int choice;
+  char link;
+  
+ 
+    printf("Enter Twitch Link: ");
+    scanf(" %s", link);
+    
+    while (choice != )
+    {
+      
+    }
+  
+}
