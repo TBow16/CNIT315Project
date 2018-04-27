c5ee554e74c640ac68ba2670e92aaca573eaa011
TBow16 <trevdog16@gmail.com>
2018-04-26 20:57:23 -0400

    Update source.c

--- source.c
+++ source.c
@@ -92,8 +92,14 @@ int main()
       printf("Enter choice: \n");
       scanf("%d", &choice);
       
-      if (1 == choice)
-      {
+      while (choice < 1 || choice > 6)
+    {
+        printf("Value out of range (%d). Try Again.", choice);
+        printf("Your choice (1-5): \n");
+        scanf(" %d", &choice);
+    }
+    if (1 == choice)
+    {
             if(list == NULL)
             {
                 printf("Error: Create a List \n \n");
@@ -112,25 +118,92 @@ int main()
 
                 InsertFront(list, username, message, user.count); 
             }
-      }
-      else if (2 == choice)
-      {
+    }
+    else if (2 == choice)
+    {
         Traverse(list);
-      }
-      else if (3 == choice)
-      {
+    }
+    else if (3 == choice)       /* Sorting */
+    {
+        choice = 0;
+        while(choice != 3)
+        {
+            printf("╔════════════════════════════╗\n");
+            printf("║           |MENU|           ║\n");
+            printf("║━━━━━━━━━━━━━━━━━━━━━━━━━━━━║\n");
+            printf("║ 1. Bubble Sort             ║\n");
+            printf("║ 2. Insertion Sort          ║\n");
+            printf("║ 3. Exit                    ║\n");
+            printf("╚════════════════════════════╝\n \n");
+            printf("Enter choice: \n");
+            scanf("%d", &choice);
+
+            while (choice < 1 || choice > 3)
+            {
+                printf("Value out of range (%d). Try Again.", choice);
+                printf("Your choice (1-3): \n");
+                scanf(" %d", &choice);
+            }
+            if (1 == choice)
+            {
+            }
+            else if (2 == choice)
+            {
+            }
+            else if (3 == choice)
+            {
+                break;
+            }
+        }
+
+    }
+    else if (4 == choice)       /* Searching */
+    {
+        printf(" ┏(-_-)┛┗(-_-)┓┗(-_-)┛┏(-_-)┓ \n");
+
+        choice = 0;
+        while(choice != 3)
+        {
+            printf("╔════════════════════════════╗\n");
+            printf("║           |MENU|           ║\n");
+            printf("║━━━━━━━━━━━━━━━━━━━━━━━━━━━━║\n");
+            printf("║ 1. Linear Search           ║\n");
+            printf("║ 2. Binary Search           ║\n");
+            printf("║ 3. Exit                    ║\n");
+            printf("╚════════════════════════════╝\n \n");
+            printf("Enter choice: \n");
+            scanf("%d", &choice);
+
+            while (choice < 1 || choice > 3)
+            {
+                printf("Value out of range (%d). Try Again.", choice);
+                printf("Your choice (1-3): \n");
+                scanf(" %d", &choice);
+            }
+            if (1 == choice)
+            {
+            }
+            else if (2 == choice)
+            {
+            }
+            else if (3 == choice)
+            {
+                break;
+            }
+        }
+    }
+    else if (5 == choice)
+    {
         printf(" ( ͡° ͜ʖ ͡°) \n");
         list = CreateList();
         printf("List has been created \n");
-      }
-      else if (4 == choice)
-      {
-        printf(" ┏(-_-)┛┗(-_-)┓┗(-_-)┛┏(-_-)┓ \n");
-      }
-      else if (5 == choice)
-      {
+    }
+    else if (6 == choice)
+    {
+        printf("Yeeeeeeet! \n\n");
         return 0;
-      }
+    }
+    }
     }
   
 }
